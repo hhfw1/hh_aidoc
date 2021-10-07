bf9aa3731e83be99af969d6fdbd9b3bdfed39147
hhfw1 <91369228+hhfw1@users.noreply.github.com>
2021-10-08 00:22:05 +0530

    Add files via upload

---
+++ hh_aidoc/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'HHFW AI Doc'
+
+version '0.1.0'
+
+
+client_scripts {
+    'config.lua',
+    'client.lua',
+  --  '@es_extended/imports.lua',  --Enable this if ur using ESX
+    '@qb-core/import.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server.lua',
+ --   '@es_extended/imports.lua', --Enable this if ur using ESX
+    '@qb-core/import.lua'
+}
---
+++ hh_aidoc/server.lua
@@ -0,0 +1,76 @@
+local isESX =  GetResourceState('es_extended') == 'started' or GetResourceState('extendedmode') == 'started'
+local isQB =  GetResourceState('qb-core') == 'started'
+QBCore = nil
+TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+
+if isQB then
+	QBCore.Functions.CreateCallback('hhfw:docOnline', function(source, cb)
+		local src = source
+		local Ply = QBCore.Functions.GetPlayer(src)
+		local xPlayers = QBCore.Functions.GetPlayers()
+		local doctor = 0
+		local canpay = false
+		if Ply.PlayerData.money["cash"] >= Config.Price then
+			canpay = true
+		else
+			if Ply.PlayerData.money["bank"] >= Config.Price then
+				canpay = true
+			end
+		end
+
+		for i=1, #xPlayers, 1 do
+			local xPlayer = QBCore.Functions.GetPlayer(xPlayers[i])
+			if xPlayer.PlayerData.job.name == 'ambulance' then
+				doctor = doctor + 1
+			end
+		end
+
+		cb(doctor, canpay)
+	end)
+elseif isESX then
+	ESX.RegisterServerCallback('hhfw:docOnline' , function(source, cb)
+		local src = source
+		local Ply = ESX.GetPlayerFromId(src)
+		local xPlayers = ESX.GetPlayers()
+		local doctor = 0
+		local canpay = false
+		if Ply.getMoney() >= Config.Price then
+			canpay = true
+		else
+			if Ply.getAccount('bank').money >= Config.Price then
+				canpay = true
+			end
+		end
+
+		for i=1, #xPlayers, 1 do
+			local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+			if xPlayer.job.name == 'ambulance' then
+				doctor = doctor + 1
+			end
+		end
+
+		cb(doctor, canpay)
+	end)
+end
+
+
+RegisterServerEvent('hhfw:charge')
+AddEventHandler('hhfw:charge', function()
+	local src = source
+	if isQB then
+		local xPlayer = QBCore.Functions.GetPlayer(src)
+		if xPlayer.PlayerData.money["cash"] >= Config.Price then
+			xPlayer.Functions.RemoveMoney("cash", Config.Price)
+		else
+			xPlayer.Functions.RemoveMoney("bank", Config.Price)
+		end
+		TriggerEvent("qb-bossmenu:server:addAccountMoney", 'ambulance', Config.Price)
+	elseif isESX then
+		local xPlayer = ESX.GetPlayerFromId(src)
+		if getMoney() >= Config.Price then
+			xPlayer.removeMoney(Config.Price)
+		else
+			xPlayer.removeAccountMoney('bank', Config.Price)
+		end
+	end
+end)
