bb477c4f6a92cac18e65ebe4fe847d1478240ace
hhfw1 <91369228+hhfw1@users.noreply.github.com>
2021-10-09 22:46:15 +0530

    Update fxmanifest.lua

--- hh_aidoc/fxmanifest.lua
+++ hh_aidoc/fxmanifest.lua
@@ -9,14 +9,10 @@ version '0.1.0'
 
 client_scripts {
     'config.lua',
-    'client.lua',
-  --  '@es_extended/imports.lua',  --Enable this if ur using ESX
-    '@qb-core/import.lua'
+    'client.lua'
 }
 
 server_scripts {
     'config.lua',
-    'server.lua',
- --   '@es_extended/imports.lua', --Enable this if ur using ESX
-    '@qb-core/import.lua'
+    'server.lua'
 }
