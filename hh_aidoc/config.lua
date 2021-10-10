32c8bd8252acc6b23775dfedc46ae75e480a0f4d
hhfw1 <91369228+hhfw1@users.noreply.github.com>
2021-10-10 09:29:03 +0530

    Delete hh_aidoc directory

--- hh_aidoc/config.lua
+++
@@ -1,9 +0,0 @@
--------------------
--- CONFIG --
--------------------
-Config = {}
-
-Config.Doctor = 0 -- Minimum Amount of EMS to work
-Config.Price = 2000
-Config.ReviveTime = 20000  --in msec
-
