96b084a5b79ee86e3ff3884939756a913a834f22
nidhin45 <72497418+nidhin45@users.noreply.github.com>
2021-10-11 14:39:45 +0530

    I changed some
    i was changed progress bar to mythic progress bar , added advanced notification, changed command to a specified key ("G") and fixed progress bar working on load time

--- hh_aidoc[ESX]/client.lua
+++ hh_aidoc[ESX]/client.lua
@@ -1,20 +1,17 @@
-local ESX = nil
-
-Citizen.CreateThread(function()
-	while ESX == nil do
-	   TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-	   Citizen.Wait(200)
-	end
-end)
-
+ESX = nil
 local Active = false
 local test = nil
 local test1 = nil
 local Act = true
 local spam = true
-
 local isDead = false
 
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(200)
+    end
+end)
 
 AddEventHandler('playerSpawned', function(spawn)
 	isDead = false
@@ -22,30 +19,33 @@ end)
 
 AddEventHandler('esx:onPlayerDeath', function(data)
 	isDead = true
-end)	
+end)
 
 
-
-RegisterCommand("help", function(source, args, raw)
-	if isDead and spam then
-		ESX.TriggerServerCallback('hhfw:docOnline', function(EMSOnline, hasEnoughMoney)
-
-			if EMSOnline <= Config.Doctor and hasEnoughMoney and spam then
-				SpawnVehicle(GetEntityCoords(PlayerPedId()))
-				TriggerServerEvent('hhfw:charge')
-				Notify("Medic is arriving")
-			else
-				if EMSOnline > Config.Doctor then
-					Notify("There is too many medics online", "error")
-				elseif not hasEnoughMoney then
-					Notify("Not Enough Money")
-				else
-					Notify("Wait Paramadic is on its Way")
-				end	
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if isDead and spam then
+			if IsControlJustPressed(1, 47) then
+				ESX.TriggerServerCallback('hhfw:docOnline', function(EMSOnline, hasEnoughMoney)
+					if EMSOnline <= Config.Doctor and hasEnoughMoney and spam then
+						SpawnVehicle(GetEntityCoords(PlayerPedId()))
+						TriggerServerEvent('hhfw:charge')
+						ShowAdvNotification("CHAR_PA_FEMALE", "Doctor", "", "There is no EMS in the city don't worry i will send my PA for you!")
+						--Notify("Medic is arriving")
+					else
+						if EMSOnline > Config.Doctor then
+							exports['CRP_Notify']:Alert("Doctor", "We got your signal, please wait", 5000, 'bell')
+							--Notify("There is too many medics online", "error")
+						elseif not hasEnoughMoney then
+							Notify("Not Enough Money")
+						else
+							Notify("Wait Paramadic is on its Way")
+						end	
+					end
+				end)
 			end
-		end)
-	else
-		Notify("This can only be used when dead")
+		end
 	end
 end)
 
@@ -122,19 +122,43 @@ function DoctorNPC()
 	end
 
 	TaskPlayAnim(test1, "mini@cpr@char_a@cpr_str","cpr_pumpchest",1.0, 1.0, -1, 9, 1.0, 0, 0, 0)
-	exports['progressBars']:startUI(Config.ReviveTime, "The doctor is giving you medical aid")
+	TriggerEvent("mythic_progbar:client:progress", {
+        name = "unique_action_name",
+        duration = 10000,
+        label = "Giving CPR.",
+        useWhileDead = true,
+        canCancel = false,
+        controlDisables = {
+            disableMovement = true,
+            disableCarMovement = true,
+            disableMouse = false,
+            disableCombat = true,
+        }
+    }, function(status)
+        if not status then
+            -- Do Something If Event Wasn't Cancelled
+        end
+    end)
 	Wait(Config.ReviveTime)
 	ClearPedTasks(test1)
 	Citizen.Wait(500)
-	TriggerEvent('esx_ambulancejob:revive')
-	StopScreenEffect('DeathFailOut')	
-	Notify("Your treatment is done, you were charged: "..Config.Price)
-	RemovePedElegantly(test1)
-	DeleteEntity(test)
+	TriggerEvent('esx_ambulancejob:revive', GetPlayerServerId(PlayerId()))
+	ShowAdvNotification("CHAR_PA_FEMALE", "Doctor", "", "Your treatment is done, you were charged : ~g~ $"..Config.Price)
+	--Notify("Your treatment is done, you were charged: "..Config.Price)
+	Citizen.Wait(5000)
+	DeletePed(test1)
+	DeleteVehicle(test)
 	spam = true
 end
 
 
-function Notify(msg)
+function Notify(msg, state)
     ESX.ShowNotification(msg)
+end
+
+function ShowAdvNotification(image, title, subtitle, text)
+	SetNotificationTextEntry("STRING");
+	AddTextComponentString(text);
+	SetNotificationMessage(image, image, false, 0, title, subtitle);
+	DrawNotification(false, true);
 end
