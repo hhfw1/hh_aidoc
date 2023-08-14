59080d023ade66587ee323b59c712cd1caaa6f09
Ron <114441702+Ronyboy4571@users.noreply.github.com>
2023-08-14 09:05:08 -0400

    Update config.lua

--- hh_aidoc[QB]/config.lua
+++ hh_aidoc[QB]/config.lua
@@ -6,4 +6,4 @@ Config = {}
 Config.Doctor = 0 -- Minimum Amount of EMS to work
 Config.Price = 2000
 Config.ReviveTime = 20000  --in msec
-
+Config.scenes = "true"   -- False = Base resources True = Timer during scene Ron = some edits I threw in plan to make more
